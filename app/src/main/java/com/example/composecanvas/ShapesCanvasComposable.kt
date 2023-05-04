4c93cd372e62387ef10e95bd908b894ffc5b6fcc
BharatIvan <developer.ivanweb@gmail.com>
2023-05-04 18:29:32 +0530

    Canvas ShapesCanvasComposable.kt added

--- app/src/main/java/com/example/composecanvas/ShapesCanvasComposable.kt
+++ app/src/main/java/com/example/composecanvas/ShapesCanvasComposable.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ShapeCanvasComposable() {
+fun ShapeCanvasComposableRect() {
     Column(modifier = Modifier.fillMaxSize()) {
         Canvas(modifier = Modifier.fillMaxWidth().fillMaxHeight(0.5f))
         {
